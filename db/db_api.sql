f18116531e86357972d7032aca634b16310905d6
Ovan Pulu <ovan89@gmail.com>
2014-09-04 23:07:25 +0700

    first commit

---
+++ db/db_api.sql
@@ -0,0 +1,221 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 01, 2014 at 04:09 PM
+-- Server version: 5.5.37-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_log`
+--
+
+CREATE TABLE IF NOT EXISTS `activity_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `activityId` int(11) NOT NULL,
+  `activityDesc` text NOT NULL,
+  `source` varchar(20) NOT NULL,
+  `datetimes` datetime NOT NULL,
+  `n_status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_category`
+--
+
+CREATE TABLE IF NOT EXISTS `api_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(200) DEFAULT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `relation` int(11) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `n_status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_member_interest`
+--
+
+CREATE TABLE IF NOT EXISTS `api_member_interest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL DEFAULT '0',
+  `category_id` int(11) NOT NULL DEFAULT '0',
+  `date_join` varchar(300) NOT NULL,
+  `n_status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_news_content`
+--
+
+CREATE TABLE IF NOT EXISTS `api_news_content` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `parentid` int(11) NOT NULL,
+  `lid` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `brief` text NOT NULL,
+  `content` text NOT NULL,
+  `image` varchar(200) NOT NULL,
+  `thumbnail_image` varchar(200) NOT NULL,
+  `slider_image` varchar(200) NOT NULL,
+  `categoryid` int(11) NOT NULL,
+  `articleType` int(11) NOT NULL COMMENT '1:news,2:profil,3:publikasi',
+  `url` varchar(200) NOT NULL,
+  `sourceurl` varchar(100) NOT NULL,
+  `file` varchar(200) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `expired_date` datetime NOT NULL,
+  `posted_date` datetime NOT NULL,
+  `fromwho` int(11) NOT NULL DEFAULT '0' COMMENT '0:web or admin ;1:user ;2:pages',
+  `filesize` int(11) NOT NULL,
+  `can_save` int(11) NOT NULL,
+  `tags` text NOT NULL COMMENT 'format serialize tags',
+  `authorid` int(11) NOT NULL,
+  `n_status` int(11) NOT NULL DEFAULT '0',
+  `topcontent` int(11) NOT NULL DEFAULT '0' COMMENT '0;standart;1:featured;2:review;3:interview',
+  PRIMARY KEY (`id`),
+  KEY `title` (`title`),
+  KEY `categoryid` (`categoryid`),
+  KEY `created_date` (`created_date`),
+  KEY `posted_date` (`posted_date`),
+  KEY `n_status` (`n_status`),
+  KEY `articleTypeID` (`articleType`),
+  KEY `image` (`image`),
+  KEY `parentID` (`parentid`),
+  KEY `lid` (`lid`),
+  KEY `online` (`fromwho`),
+  KEY `expired_date` (`expired_date`),
+  KEY `url` (`url`),
+  KEY `aid` (`authorid`),
+  KEY `file` (`file`),
+  KEY `slider_image` (`slider_image`),
+  KEY `sourceurl` (`filesize`),
+  KEY `thumbnail_image` (`thumbnail_image`),
+  KEY `topcontent` (`topcontent`),
+  KEY `sourceurl_2` (`sourceurl`),
+  KEY `can_save` (`can_save`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_news_content_comment`
+--
+
+CREATE TABLE IF NOT EXISTS `api_news_content_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `contentid` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `date` datetime NOT NULL,
+  `n_status` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `date` (`date`),
+  KEY `userid` (`userid`),
+  KEY `contentid` (`contentid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_news_content_repo`
+--
+
+CREATE TABLE IF NOT EXISTS `api_news_content_repo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `brief` text NOT NULL,
+  `content` text NOT NULL,
+  `typealbum` int(11) NOT NULL COMMENT '1:song;2:images;3:video',
+  `gallerytype` int(11) NOT NULL COMMENT '0:content,1:contest',
+  `files` varchar(200) NOT NULL COMMENT 'can be image or song',
+  `thumbnail` varchar(200) NOT NULL,
+  `fromwho` int(11) NOT NULL COMMENT '0;admin;1:user;2:pages',
+  `otherid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `created_date` datetime NOT NULL,
+  `n_status` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `otherid` (`otherid`),
+  KEY `IDX_typeAlbum` (`typealbum`),
+  KEY `IDX_Album_ID` (`gallerytype`),
+  KEY `IDX_FROM_WHO` (`fromwho`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_member`
+--
+
+CREATE TABLE IF NOT EXISTS `social_member` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `name` varchar(46) DEFAULT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `verified_date` datetime NOT NULL,
+  `img` varchar(200) DEFAULT NULL COMMENT 'GIID Image',
+  `image_profile` varchar(200) NOT NULL,
+  `small_img` varchar(200) DEFAULT NULL,
+  `username` varchar(46) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `city` int(11) DEFAULT NULL,
+  `zipcode` int(10) NOT NULL,
+  `sex` varchar(11) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `description` text,
+  `middle_name` varchar(46) DEFAULT NULL,
+  `last_name` varchar(46) DEFAULT NULL,
+  `StreetName` varchar(150) DEFAULT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `n_status` int(3) NOT NULL DEFAULT '0' COMMENT ' pending , approved, verified, rejected , deleted ( 7 day ), deactivated ( kill my self )',
+  `login_count` int(11) NOT NULL DEFAULT '0',
+  `verified` tinyint(3) DEFAULT '0' COMMENT '0->no hp blm verified, 1->sudah verified.',
+  `usertype` int(11) NOT NULL COMMENT '0:online;1:offline;2;existing',
+  `email_token` varchar(50) DEFAULT NULL,
+  `photo_moderation` int(11) NOT NULL,
+  `salt` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10044 ;
+
+--
+-- Dumping data for table `social_member`
+--
+
+INSERT INTO `social_member` (`id`, `name`, `nickname`, `email`, `register_date`, `verified_date`, `img`, `image_profile`, `small_img`, `username`, `last_login`, `city`, `zipcode`, `sex`, `birthday`, `description`, `middle_name`, `last_name`, `StreetName`, `phone_number`, `n_status`, `login_count`, `verified`, `usertype`, `email_token`, `photo_moderation`, `salt`, `password`) VALUES
+(10043, 'ovan', NULL, 'ovan89@gmail.com', '2014-07-16 07:53:04', '0000-00-00 00:00:00', NULL, '', NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, 0, NULL, 0, NULL, NULL);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
