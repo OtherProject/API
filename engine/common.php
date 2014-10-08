c6407ae35a07b6b8309293842df89a3ab60c82b1
Ovan Pulu <ovan89@gmail.com>
2014-10-09 06:08:07 +0700

    revisi common

--- engine/common.php
+++ engine/common.php
@@ -10,28 +10,14 @@ function base_url() {
 	return $base_url;
 }
 
-function dateFormat($date,$type=false,$locale='id_ID.utf8'){
-		
-		/* ex : dateFormat(<any type of date>, 'dd-mm-yyyy') */
-
-		if($date =='') exit('Date not complete');
-
-		setlocale (LC_TIME, 'id_ID.utf8');
-		
-		if($type == 'dd-mm-yyyy'){
-			return strftime( "%d-%m-%Y", strtotime($date));
-		} 
-		if($type == 'dd/mm/yyyy'){
-			return strftime( "%d/%m/%Y", strtotime($date));
-		} 
-		elseif ($type == 'article-day') {
-			return strftime( "%A, %d %B %Y", strtotime($date));
-		}
-		elseif ($type == 'article') {
-			return strftime( "%d %B %Y", strtotime($date));
-		}
-	}
+function changeDate($date=false)
+{
+	if (!$date) return false;
+	$changeFormat = date("j F Y",strtotime($date));
 	
+	return $changeFormat;
+}
+
 function change_date_simple($date_data, $type, $order_by)
 {
 	/* ex : change_date_to_slash('2012-01-01', 'slash', 'by_date') */
@@ -112,8 +98,44 @@ function under_development() {
 
 function redirect($data) {
 	
-	echo "<meta http-equiv=\"Refresh\" content=\"0; url={$data}\">";
+	echo "<meta http-equiv=\"refresh\" content=\"0; url={$data}\">";
 
+}
+
+
+function imageFrame($filename=false, $framefile=false)
+{
+
+	global $IMAGE, $CONFIG;
+
+	if (array_key_exists('mobile',$CONFIG)) include(APP.LIBS.'class_image_upload/class.upload.php');
+	else include(LIBS.'class_image_upload/class.upload.php');
+
+	deleteFile($filename,'imageFramed');
+	// pr($IMAGE[0]['pathfile'].$filename);
+	$handle = new Upload($IMAGE[0]['pathfile'].$filename);
+	// pr($handle);
+	if ($handle->uploaded) {
+		$handle->image_resize = true;
+		$handle->image_x = 180;
+		$handle->image_y = 181;
+		$handle->image_ratio_crop = false;
+		$handle->jpeg_quality = 100;
+		$handle->image_watermark = $IMAGE[0]['pathframe'].$framefile;
+
+		$handle->Process($IMAGE[0]['imageframed']);
+		if ($handle->processed) {
+			$filename = $handle->file_dst_name;
+		}else{
+			echo 'Error: ' . $handle->error . '';
+		}
+		$handle-> Clean();
+		return true;
+	}else{
+		echo 'Error: ' . $handle->error . '';
+	}
+	
+	return false;
 }
 
 /**
@@ -133,7 +155,7 @@ function uploadFile($data,$path=null,$ext){
 	
 	if (array_key_exists('admin',$CONFIG)) $key = 'admin';
 	if (array_key_exists('default',$CONFIG)) $key = 'default';
-    
+    if (array_key_exists('mobile',$CONFIG)) $key = 'mobile';
     /* result template
     $result = array(
         'status' => '',
@@ -157,6 +179,8 @@ function uploadFile($data,$path=null,$ext){
         return $result;
     }
 	
+	logFile(serialize($_FILES[$data]));
+
 	if ($path!='') $path = $path.'/';
 	$pathFile = $CONFIG[$key]['upload_path'].$path;
 	$ext = explode ('.',$_FILES[$data]["name"]);
@@ -165,6 +189,10 @@ function uploadFile($data,$path=null,$ext){
 	$shufflefilename = md5(str_shuffle('codekir-v0.3'.$CONFIG[$key]['max_filesize']));
 	$filename = $shufflefilename.'.'.$getExt;
 	
+	/* Host Folder path */
+	list($root_path, $dummy) = explode('admin',$CONFIG[$key]['root_path']);
+	list($dummy, $pathFolder) = explode($root_path,$pathFile);
+
 	if ($_FILES[$data]["error"] > 0){
 	
 		echo "Return Code: " . $_FILES[$data]["error"] . "<br>";
@@ -175,17 +203,18 @@ function uploadFile($data,$path=null,$ext){
 		($_FILES[$data]["size"] / $CONFIG[$key]['max_filesize']);
 		$_FILES[$data]["tmp_name"];
 
-		if (file_exists($pathFile. $_FILES[$data]["name"])){
-			$result = array(
-				'status' => '0',
-				'message' => 'File exist.',
-				'full_path' => $pathFile,
-				'full_name' => $filename,
-				'raw_name' => $shufflefilename,
-                'real_name' => $_FILES[$data]["name"]
-			);
-			return $result;
-		}else{
+		// if (file_exists($pathFile. $_FILES[$data]["name"])){
+		// 	$result = array(
+		// 		'status' => '0',
+		// 		'message' => 'File exist.',
+		// 		'full_path' => $pathFile,
+		// 		'full_name' => $filename,
+		// 		'raw_name' => $shufflefilename,
+  //               'real_name' => $_FILES[$data]["name"],
+  //               'folder_name' => $pathFolder
+		// 	);
+		// 	return $result;
+		// }else{
 		
 			move_uploaded_file($_FILES[$data]["tmp_name"],$pathFile . $filename);
 			$result = array(
@@ -194,13 +223,37 @@ function uploadFile($data,$path=null,$ext){
 				'full_path' => $pathFile,
 				'full_name' => $filename,
 				'raw_name' => $shufflefilename,
-                'real_name' => $_FILES[$data]["name"]
+                'real_name' => $_FILES[$data]["name"],
+                'folder_name' => $pathFolder
 			);
+
+			// pr($result);exit;
 			return $result;
-		}
+		// }
 	}
 	
 	return $filename;
+}
+
+function deleteFile($data=null, $path=null)
+{	
+	global $CONFIG;
+	
+	if (array_key_exists('admin',$CONFIG)) $key = 'admin';
+	if (array_key_exists('default',$CONFIG)) $key = 'default';
+	if (array_key_exists('mobile',$CONFIG)) $key = 'mobile';
+	
+	if ($data == null) return false;
+	if ($path!='') $data = $path.'/'.$data;	
+	
+	$fileName = $CONFIG[$key]['upload_path'].$data;
+	
+	if (is_file($fileName)){
+		unlink($fileName);
+	}else{
+		return false;
+	}
+	
 }
 
 function encode($data=false)
@@ -427,6 +480,28 @@ function createAccount($data=array())
 
 }
 
+function dateFormat($date,$type=false,$locale='id_ID.utf8'){
+		
+		/* ex : dateFormat(<any type of date>, 'dd-mm-yyyy') */
+
+		if($date =='') exit('Date not complete');
+
+		setlocale (LC_TIME, 'id_ID.utf8');
+		
+		if($type == 'dd-mm-yyyy'){
+			return strftime( "%d-%m-%Y", strtotime($date));
+		} 
+		if($type == 'dd/mm/yyyy'){
+			return strftime( "%d/%m/%Y", strtotime($date));
+		} 
+		elseif ($type == 'article-day') {
+			return strftime( "%A, %d %B %Y", strtotime($date));
+		}
+		elseif ($type == 'article') {
+			return strftime( "%d %B %Y", strtotime($date));
+		}
+	}
+
 function sendGlobalMail($to,$from,$msg,$config=true){
 
 
@@ -479,17 +554,200 @@ function sendGlobalMail($to,$from,$msg,$config=true){
 	
 }
 
-function decodeJson($url=false)
+function smart_resize_image($file,
+                              $width = 0,
+                              $height = 0,
+                              $proportional = false,
+                              $output = 'file',
+                              $delete_original = true,
+                              $use_linux_commands = false,
+   $quality = 100
+   ) {
+      
+    if ( $height <= 0 && $width <= 0 ) return false;
+
+    # Setting defaults and meta
+    $info = getimagesize($file);
+    $image = '';
+    $final_width = 0;
+    $final_height = 0;
+    list($width_old, $height_old) = $info;
+$cropHeight = $cropWidth = 0;
+
+    # Calculating proportionality
+    if ($proportional) {
+      if ($width == 0) $factor = $height/$height_old;
+      elseif ($height == 0) $factor = $width/$width_old;
+      else $factor = min( $width / $width_old, $height / $height_old );
+
+      $final_width = round( $width_old * $factor );
+      $final_height = round( $height_old * $factor );
+    }
+    else {
+		$final_width = ( $width <= 0 ) ? $width_old : $width;
+		$final_height = ( $height <= 0 ) ? $height_old : $height;
+		$widthX = $width_old / $width;
+		$heightX = $height_old / $height;
+
+		$x = min($widthX, $heightX);
+		$cropWidth = ($width_old - $width * $x) / 2;
+		$cropHeight = ($height_old - $height * $x) / 2;
+    }
+	
+    # Loading image to memory according to type
+    switch ( $info[2] ) {
+      case IMAGETYPE_JPEG: $image = imagecreatefromjpeg($file); break;
+      case IMAGETYPE_GIF: $image = imagecreatefromgif($file); break;
+      case IMAGETYPE_PNG: $image = imagecreatefrompng($file); break;
+      default: return false;
+    }
+    
+    
+    # This is the resizing/resampling/transparency-preserving magic
+    $image_resized = imagecreatetruecolor( $final_width, $final_height );
+    if ( ($info[2] == IMAGETYPE_GIF) || ($info[2] == IMAGETYPE_PNG) ) {
+      $transparency = imagecolortransparent($image);
+      $palletsize = imagecolorstotal($image);
+
+      if ($transparency >= 0 && $transparency < $palletsize) {
+        $transparent_color = imagecolorsforindex($image, $transparency);
+        $transparency = imagecolorallocate($image_resized, $transparent_color['red'], $transparent_color['green'], $transparent_color['blue']);
+        imagefill($image_resized, 0, 0, $transparency);
+        imagecolortransparent($image_resized, $transparency);
+      }
+      elseif ($info[2] == IMAGETYPE_PNG) {
+        imagealphablending($image_resized, false);
+        $color = imagecolorallocatealpha($image_resized, 0, 0, 0, 127);
+        imagefill($image_resized, 0, 0, $color);
+        imagesavealpha($image_resized, true);
+      }
+    }
+    imagecopyresampled($image_resized, $image, 0, 0, $cropWidth, $cropHeight, $final_width, $final_height, $width_old - 2 * $cropWidth, $height_old - 2 * $cropHeight);
+
+
+    # Taking care of original, if needed
+    if ( $delete_original ) {
+      if ( $use_linux_commands ) exec('rm '.$file);
+      else @unlink($file);
+    }
+
+    # Preparing a method of providing result
+    switch ( strtolower($output) ) {
+      case 'browser':
+        $mime = image_type_to_mime_type($info[2]);
+        header("Content-type: $mime");
+        $output = NULL;
+      break;
+      case 'file':
+        $output = $file;
+      break;
+      case 'return':
+        return $image_resized;
+      break;
+      default:
+      break;
+    }
+    
+    # Writing image according to type to the output destination and image quality
+    switch ( $info[2] ) {
+      case IMAGETYPE_GIF: imagegif($image_resized, $output); break;
+      case IMAGETYPE_JPEG: imagejpeg($image_resized, $output, $quality); break;
+      case IMAGETYPE_PNG:
+        $quality = 9 - (int)((0.9*$quality)/10.0);
+        imagepng($image_resized, $output, $quality);
+        break;
+      default: return false;
+    }
+
+    return true;
+}
+
+function ImageCreateFromBMP($filename)
 {
-	
-	if (!$url) return false;
-	$content = file_get_contents($url);
-	if ($content){
-		$decodeJson = json_decode($content);
-		return $decodeJson;	
-	}
-	return false;
-	
+//Ouverture du fichier en mode binaire
+   if (! $f1 = fopen($filename,"rb")) return FALSE;
+
+//1 : Chargement des ent�tes FICHIER
+   $FILE = unpack("vfile_type/Vfile_size/Vreserved/Vbitmap_offset", fread($f1,14));
+   if ($FILE['file_type'] != 19778) return FALSE;
+
+//2 : Chargement des ent�tes BMP
+   $BMP = unpack('Vheader_size/Vwidth/Vheight/vplanes/vbits_per_pixel'.
+                 '/Vcompression/Vsize_bitmap/Vhoriz_resolution'.
+                 '/Vvert_resolution/Vcolors_used/Vcolors_important', fread($f1,40));
+   $BMP['colors'] = pow(2,$BMP['bits_per_pixel']);
+   if ($BMP['size_bitmap'] == 0) $BMP['size_bitmap'] = $FILE['file_size'] - $FILE['bitmap_offset'];
+   $BMP['bytes_per_pixel'] = $BMP['bits_per_pixel']/8;
+   $BMP['bytes_per_pixel2'] = ceil($BMP['bytes_per_pixel']);
+   $BMP['decal'] = ($BMP['width']*$BMP['bytes_per_pixel']/4);
+   $BMP['decal'] -= floor($BMP['width']*$BMP['bytes_per_pixel']/4);
+   $BMP['decal'] = 4-(4*$BMP['decal']);
+   if ($BMP['decal'] == 4) $BMP['decal'] = 0;
+
+//3 : Chargement des couleurs de la palette
+   $PALETTE = array();
+   if ($BMP['colors'] < 16777216)
+   {
+    $PALETTE = unpack('V'.$BMP['colors'], fread($f1,$BMP['colors']*4));
+   }
+
+//4 : Cr�ation de l'image
+   $IMG = fread($f1,$BMP['size_bitmap']);
+   $VIDE = chr(0);
+
+   $res = imagecreatetruecolor($BMP['width'],$BMP['height']);
+   $P = 0;
+   $Y = $BMP['height']-1;
+   while ($Y >= 0)
+   {
+    $X=0;
+    while ($X < $BMP['width'])
+    {
+     if ($BMP['bits_per_pixel'] == 24)
+        $COLOR = unpack("V",substr($IMG,$P,3).$VIDE);
+     elseif ($BMP['bits_per_pixel'] == 16)
+     {  
+        $COLOR = unpack("n",substr($IMG,$P,2));
+        $COLOR[1] = $PALETTE[$COLOR[1]+1];
+     }
+     elseif ($BMP['bits_per_pixel'] == 8)
+     {  
+        $COLOR = unpack("n",$VIDE.substr($IMG,$P,1));
+        $COLOR[1] = $PALETTE[$COLOR[1]+1];
+     }
+     elseif ($BMP['bits_per_pixel'] == 4)
+     {
+        $COLOR = unpack("n",$VIDE.substr($IMG,floor($P),1));
+        if (($P*2)%2 == 0) $COLOR[1] = ($COLOR[1] >> 4) ; else $COLOR[1] = ($COLOR[1] & 0x0F);
+        $COLOR[1] = $PALETTE[$COLOR[1]+1];
+     }
+     elseif ($BMP['bits_per_pixel'] == 1)
+     {
+        $COLOR = unpack("n",$VIDE.substr($IMG,floor($P),1));
+        if     (($P*8)%8 == 0) $COLOR[1] =  $COLOR[1]        >>7;
+        elseif (($P*8)%8 == 1) $COLOR[1] = ($COLOR[1] & 0x40)>>6;
+        elseif (($P*8)%8 == 2) $COLOR[1] = ($COLOR[1] & 0x20)>>5;
+        elseif (($P*8)%8 == 3) $COLOR[1] = ($COLOR[1] & 0x10)>>4;
+        elseif (($P*8)%8 == 4) $COLOR[1] = ($COLOR[1] & 0x8)>>3;
+        elseif (($P*8)%8 == 5) $COLOR[1] = ($COLOR[1] & 0x4)>>2;
+        elseif (($P*8)%8 == 6) $COLOR[1] = ($COLOR[1] & 0x2)>>1;
+        elseif (($P*8)%8 == 7) $COLOR[1] = ($COLOR[1] & 0x1);
+        $COLOR[1] = $PALETTE[$COLOR[1]+1];
+     }
+     else
+        return FALSE;
+     imagesetpixel($res,$X,$Y,$COLOR[1]);
+     $X++;
+     $P += $BMP['bytes_per_pixel'];
+    }
+    $Y--;
+    $P+=$BMP['decal'];
+   }
+
+	//Fermeture du fichier
+   	fclose($f1);
+
+	return $res;
 }
 
 
