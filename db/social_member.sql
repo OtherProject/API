bae3992552b2ba7bf94f13f73459f109520021e1
Ovan Pulu <ovan89@gmail.com>
2015-02-04 19:25:13 +0700

    add tabel social member

---
+++ db/social_member.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 04, 2015 at 07:24 PM
+-- Server version: 5.5.40-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_member`
+--
+
+CREATE TABLE IF NOT EXISTS `social_member` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `nip` varchar(20) DEFAULT NULL,
+  `name` varchar(46) DEFAULT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `verified_date` datetime DEFAULT NULL,
+  `img` varchar(200) DEFAULT NULL COMMENT 'Persetujuan Image',
+  `image_profile` varchar(200) NOT NULL,
+  `small_img` varchar(200) DEFAULT NULL,
+  `username` varchar(46) DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `city` int(11) DEFAULT NULL,
+  `zipcode` int(10) NOT NULL,
+  `sex` varchar(11) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `description` text,
+  `middle_name` varchar(46) DEFAULT NULL,
+  `last_name` varchar(46) DEFAULT NULL,
+  `StreetName` varchar(150) DEFAULT NULL,
+  `phone_number` varchar(15) DEFAULT NULL,
+  `pendidikan` varchar(20) DEFAULT NULL,
+  `kepakaran` varchar(100) DEFAULT NULL,
+  `alamatKantor` varchar(300) DEFAULT NULL,
+  `tlpKantor` varchar(20) DEFAULT NULL,
+  `keberhasilan` varchar(300) DEFAULT NULL,
+  `n_status` int(3) NOT NULL DEFAULT '0' COMMENT ' pending , approved, verified, rejected , deleted ( 7 day ), deactivated ( kill my self )',
+  `login_count` int(11) NOT NULL DEFAULT '0',
+  `verified` tinyint(3) DEFAULT '0' COMMENT '0->no hp blm verified, 1->sudah verified.',
+  `usertype` int(11) NOT NULL COMMENT '0:online;1:offline;2;existing',
+  `email_token` varchar(50) DEFAULT NULL,
+  `photo_moderation` int(11) NOT NULL,
+  `salt` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
